90da8f47ae8a932f10acf2e7c9730632cc3ce3dd
Jung Yuil <yi1397a@gmail.com>
2020-06-28 19:46:27 +0900

    200628 dnn 코드 작성중

--- Face_Recognition/main.cpp
+++ Face_Recognition/main.cpp
@@ -1,10 +1,23 @@
-/*
- * 시간 남으면 OpenCV v4l2로 변경할것
- */
+#include <dlib/dnn.h>
+#include <dlib/gui_widgets.h>
+#include <dlib/clustering.h>
+#include <dlib/string.h>
+#include <dlib/image_io.h>
+#include <dlib/image_processing/frontal_face_detector.h>
 
-#include "face_encode.h"
+using namespace dlib;
+using namespace std;
 
-int main()
-{
-    return 0;
-}
+template <
+    template <int, template<typename>class, int, typename> class block,
+    int N, 
+    template<typename>class BN,
+    typename SUBNET>
+using residual = add_prev1<block<N, BN, 1, tag1<SUBNET>>>;
+
+template <
+    template <int, template<typename>class, int, typename> class block, 
+    int N,
+    template<typename>class BN,
+    typename SUBNET>
+using residual_down = add_prev2<avg_pool<2, 2, 2, 2, skip1<tag2<block<N, BN, 2, tag1<SUBNET>>>>>>;
